cb681083076c87eb266bf0957a6b433401bcecee
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-26 14:09:03 +0100

    Add merchants and customers models

--- management/models.py
+++ management/models.py
@@ -1,6 +1,5 @@
 from management import db
 from flask_login import UserMixin
-from datetime import datetime
 
 
 class Users(UserMixin, db.Model):
@@ -12,6 +11,8 @@ class Users(UserMixin, db.Model):
     email_confirmed_at = db.Column(db.DateTime())
     is_active = db.Column(db.Boolean(), default=False)
     password = db.Column(db.String(300), nullable=False)
+    merchants = db.relationship('Merchants', backref='merchant', lazy=True)
+    customers = db.relationship('Customers', backref='customer', lazy=True)
 
     def __repr__(self):
         return self.business_name
@@ -19,3 +20,28 @@ class Users(UserMixin, db.Model):
     def get_date(self):
         date = self.email_confirmed_at.strftime('%B %d, %Y')
         return date
+
+
+class Merchants(db.Model):
+    """Merchants table"""
+    id = db.Column(db.Integer, primary_key=True)
+    merchant_name = db.Column(db.String(50), unique=True, nullable=False)
+    contact_name = db.Column(db.String(50))
+    email = db.Column(db.String(120), unique=True)
+    phone_number = db.Column(db.String(20), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return self.merchant_name
+
+
+class Customers(db.Model):
+    """Merchants table"""
+    id = db.Column(db.Integer, primary_key=True)
+    customer_name = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    phone_number = db.Column(db.String(20), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return self.customer_name
