71e2c1e9dda1b12d23f52f301e92cb8efc664bbd
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-26 08:24:11 +0100

    Add forms format

---
+++ management/forms.py
@@ -0,0 +1,54 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, validators, TextAreaField
+
+
+class RegisterForm(FlaskForm):
+    business_name = StringField("Business Name: ", [validators.InputRequired(message="This field cannot be empty."),
+                                                    validators.Length(min=3, max=50,
+                                                                      message="Business name must be between 3 to 50 "
+                                                                              "characters.")
+                                                    ])
+    email = StringField("Email Address: ", [validators.InputRequired(message="This field cannot be empty."),
+                                            validators.Email(message="That is not a valid email address.")])
+    password = PasswordField("Password: ", [validators.InputRequired(message="This field cannot be empty."),
+                                            validators.Length(min=8,
+                                                              message="Password must be at least 8 characters.")])
+    submit = SubmitField("Create account")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email Address: ", [validators.InputRequired(message="This field cannot be empty."),
+                                            validators.Email(message="That is not a valid email address.")])
+    password = PasswordField("Password: ", [validators.InputRequired(message="This field cannot be empty."),
+                                            validators.Length(min=8,
+                                                              message="Password must be at least 8 characters.")])
+    submit = SubmitField("Login")
+
+
+class NewEmailConfirmationRequestForm(FlaskForm):
+    email = StringField("Registered Email Address: ", [validators.InputRequired(message="This field cannot be empty."),
+                                                       validators.Email(message="That is not a valid email address.")])
+    submit = SubmitField("Submit request")
+
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField("Registered Email Address: ", [validators.InputRequired(message="This field cannot be empty."),
+                                                       validators.Email(message="That is not a valid email address.")])
+    submit = SubmitField("Submit request")
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("New Password: ", [validators.InputRequired(message="This field cannot be empty."),
+                                                validators.Length(min=8,
+                                                                  message="Password must be at least 8 characters.")])
+    submit = SubmitField("Reset password")
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField("Old Password: ", [validators.InputRequired(message="This field cannot be empty."),
+                                                    validators.Length(min=8,
+                                                                      message="Password must be at least 8 characters.")])
+    new_password = PasswordField("New Password: ", [validators.InputRequired(message="This field cannot be empty."),
+                                                    validators.Length(min=8,
+                                                                      message="Password must be at least 8 characters.")])
+    submit = SubmitField("Change password")
