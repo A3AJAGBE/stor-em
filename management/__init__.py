f19dfa961d30b3127ae9efa9a9ea8ed274049682
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-26 09:44:19 +0100

    Update commit

--- management/__init__.py
+++ management/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_mail import Mail
 import os
 from dotenv import load_dotenv
 
@@ -14,5 +16,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 Bootstrap(app)
+mail = Mail(app)
 
-from management import views
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+login_manager.login_message = "Login to access this page."
+login_manager.login_message_category = "danger"
+
+from management import views, models, errors
