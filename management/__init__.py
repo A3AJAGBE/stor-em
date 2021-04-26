6319c8458b57297c50ec5a4252f3569acef335f1
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-26 08:24:52 +0100

    Add new installed package instance

--- management/__init__.py
+++ management/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('APP_ENV'))
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+Bootstrap(app)
 
 from management import views
