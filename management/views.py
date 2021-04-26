a98f9622d2822ea2e748be385e9b3c6be83185c5
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-26 08:25:45 +0100

    Add register view

--- management/views.py
+++ management/views.py
@@ -1,7 +1,18 @@
 from management import app
+from management.forms import *
 from flask import render_template
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('auth/register.html', form=form)
+
+
