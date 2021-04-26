6bb62b64daf954aaa086c4a29c6dc8891c6c1639
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-26 08:58:12 +0100

    Add users model

---
+++ management/models.py
@@ -0,0 +1,16 @@
+from management import db
+from flask_login import UserMixin
+
+
+class Users(UserMixin, db.Model):
+    """Users table"""
+    id = db.Column(db.Integer, primary_key=True)
+    business_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    email_confirmed = db.Column(db.Boolean(), default=False)
+    email_confirmed_at = db.Column(db.DateTime())
+    is_active = db.Column(db.Boolean(), default=False)
+    password = db.Column(db.String(300), nullable=False)
+
+    def __repr__(self):
+        return self.business_name
